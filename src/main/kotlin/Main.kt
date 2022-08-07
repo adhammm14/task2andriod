4ea1a55417e44211829e31726684b301fa8f6493
Adham Misallam <adham.misallam3@gmail.com>
2022-08-07 05:18:28 +0200

    First Version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
+import java.util.*
+
 fun main() {
-    var number1 = 10
-    var number2 = 15
+    var sc = Scanner(System.`in`)
+
+    println("Enter First Number:")
+    var number1 = sc.nextInt()
+    println("Enter First Number:")
+    var number2 = sc.nextInt()
     var result = number1 + number2
-    println(result)
+    println("Result: $result")
 }
